4250b713f79bffc8ffac219122de643d241c3587
Kobe <kobeguo02@gmail.com>
2022-09-20 02:58:52 -0500

    tweaked main.py formatting

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import requests
 def find_jobs(url, unfamiliar):
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml')
-    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
     for index, job in enumerate(jobs):
-        published_date = job.find('span', class_ = 'sim-posted').span.text
+        published_date = job.find('span', class_='sim-posted').span.text
         if 'few' or 'today' or '1 day' in published_date:
-            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
             more_info = job.header.h2.a['href']
             if unfamiliar.lower() == 'nothing':
                 unfamiliar = '¤¤¤¤¤¤sdsdsfedgrghbsfc'
@@ -28,14 +28,16 @@ if __name__ == '__main__':
     while scrape_continue:
         print('Enter the website url from timesjobs.com you want to scrape')
         website_url = input('> ')
-    
+
         print('Put some skill that you are not familiar with (Put nothing if there\'s no skill unfamiliar)')
         unfamiliar_skill = input('> ').lower()
         print(f'Filtering out {unfamiliar_skill}')
 
         find_jobs(website_url, unfamiliar_skill)
 
-        question_continue = input('Do you want to scrape again? Make sure to have the posts folder empty if you do (Y/N) ')
+        question_continue = input('Do you want to scrape again? Make sure to have the posts folder empty if you do ('
+                                  'Y/N) ')
         if question_continue.lower() != 'y':
             scrape_continue = False
+
         
